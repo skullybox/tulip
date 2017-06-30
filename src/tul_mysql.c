c9095b1e6525c760a6f08807a616cdf835f320f5
Irfan Gulamali <irfan.412@yahoo.com>
2017-06-30 10:04:36 -0400

    adding mysql library

---
+++ src/tul_mysql.c
@@ -0,0 +1,25 @@
+/***
+  Copyright (C) irfan
+  mysql client
+ **/
+
+#include "tul_mysql.h"
+
+#define SQL_HST "db-vip"
+#define SQL_USR "root"
+#define SQL_PAS ""
+
+int db_connect()
+{
+
+  return 1;
+}
+
+
+int db_query(uchar *q)
+{
+
+
+  return 1;
+}
+
