bf58fa0266317fd93808959ca78bbd62f1da8169
Irfan Gulamali <irfan.412@yahoo.com>
2017-10-03 00:32:00 -0400

    adding db interface stubs

---
+++ src/tul_db.c
@@ -0,0 +1,13 @@
+/***
+  Copyright (C) irfan
+  db interface definition
+ **/
+
+
+void tul_query(char *x)
+{
+
+}
+
+
+
---
+++ src/tul_db.h
@@ -0,0 +1,13 @@
+/***
+  Copyright (C) irfan
+  database interface
+ **/
+
+#ifndef _tul_db
+#define _tul_db
+
+void tul_query(char *x);
+
+#endif
+
+
