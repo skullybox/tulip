451e36e65c1b5d68a77bbf59e7983bd21753e37d
Irfan Gulamali <irfan.412@yahoo.com>
2017-10-03 22:57:29 -0400

    update sqlite to use multi-thread during build; update stub for db queries

--- src/tul_db.c
+++ src/tul_db.c
@@ -3,11 +3,67 @@
   db interface definition
  **/
 
+#include "tul_log.h"
+#include "tul_db.h"
+#include <sqlite3.h>
+#include <stdio.h>
 
-void tul_query(char *x)
+extern int TUL_SIGNAL_INT;
+
+void tul_query(char *x,...)
 {
+  sqlite3 *db = NULL; 
+  int rc = 0;
+
+  rc = sqlite3_open(TUL_DBNAME, &db);
+
+  if(rc != SQLITE_OK)
+  {
+    tul_log("ERROR: database IO error - Exiting"); 
+    TUL_SIGNAL_INT = 1;
+    return;
+  }
+
+  /* TODO: execute queries */
 
 }
 
+void tul_init()
+{
+  /* check if db file exists */
+  FILE *fp = fopen(TUL_DBNAME, "r");
+
+  if(fp)
+  {
+    tul_log(" tulip_boot >>>> found existing database");
+    fclose(fp);
+    return;
+  }
+
+  tul_log(" tulip_boot >>>> initializing database");
+
+
+  /* open database connection
+   * this will create the empty DB file
+   */
+  sqlite3 *db = NULL; 
+  int rc = 0;
+
+  rc = sqlite3_open(TUL_DBNAME, &db);
+
+  if(rc != SQLITE_OK)
+  {
+    tul_log(" tulip_boot >>>> ERROR: db file IO error");
+    return;
+  }
+  sqlite3_close(db);
+
+  /* initialize the database at this point
+   * execute queries to create tables and initialized
+   * data.
+   */
+
+  
+}
 
 
--- src/tul_db.h
+++ src/tul_db.h
@@ -6,7 +6,10 @@
 #ifndef _tul_db
 #define _tul_db
 
-void tul_query(char *x);
+static char TUL_DBNAME[] = "tulip.sql";
+
+void tul_query(char *x,...);
+void tul_init();
 
 #endif
 
