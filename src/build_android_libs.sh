0bc913446bc4607bb716ce0e27a4427ca01fc57d
Irfan Gulamali <irfan.412@yahoo.com>
2017-10-25 10:21:12 -0400

    fixed copy error on libs

--- src/build_android_libs.sh
+++ src/build_android_libs.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+TARGET_DIR=../Android/Tulip/app/src/main/jniLibs
 
-for i in $(ls Make_*); do make -f "$i" android CL=test; done
+#for i in $(ls Make_*); do make -f $i android CL=test; done
 
+$(cp ./out/arm-linux-androideabi-4.9/*.so $TARGET_DIR/armeabi-v7a/)
+$(cp ./out/aarch64-linux-android-4.9/*.so $TARGET_DIR/arm64_v8a/)
+$(cp ./out/mipsel-linux-android-4.9/*.so $TARGET_DIR/mips/)
+$(cp ./out/mips64el-linux-android-4.9/*.so $TARGET_DIR/mips64/)
+$(cp ./out/x86-4.9/*.so $TARGET_DIR/x86/)
+$(cp ./out/x86_64-4.9/*.so $TARGET_DIR/x86_64/)
