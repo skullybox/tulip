f252a98f31c706775a5efe80fb35bc3132953deb
Irfan Gulamali <irfan.412@yahoo.com>
2017-10-25 12:14:17 -0400

    uncomment

--- src/build_android_libs.sh
+++ src/build_android_libs.sh
@@ -2,7 +2,7 @@
 
 TARGET_DIR=../Android/Tulip/app/src/main/jniLibs
 
-#for i in $(ls Make_*); do make -f $i android CL=test; done
+for i in $(ls Make_*); do make -f $i android CL=test; done
 
 $(cp ./out/arm-linux-androideabi-4.9/*.so $TARGET_DIR/armeabi-v7a/)
 $(cp ./out/aarch64-linux-android-4.9/*.so $TARGET_DIR/arm64_v8a/)
