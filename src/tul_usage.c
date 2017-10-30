9e37779ee1cdff4430c3b2e238f8f5d7ce948ff8
Irfan Gulamali <irfan.412@yahoo.com>
2017-10-30 12:04:41 -0400

    update usage

--- src/tul_usage.c
+++ src/tul_usage.c
@@ -16,7 +16,10 @@ int usage(int argc)
   {
     printf(" usage: <tulip> <port> -d\n\
         \tport: listening port\n\
-        \t  -d: run as daemon\n\n");
+        \t  -d: run as daemon\n\n\
+        \tmysql usage:\n\
+        \t set the following environment variables:\n\
+        \t MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD\n\n");
     return 1;
   }
   return 0;
