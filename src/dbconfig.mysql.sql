61149ce9e6b1a6875e6642b3789f71750079b203
Irfan Gulamali <irfan.412@yahoo.com>
2017-11-04 14:29:59 -0400

    update mysql script

--- src/dbconfig.mysql.sql
+++ src/dbconfig.mysql.sql
@@ -1,12 +1,12 @@
 # database configuration settings
 
-CREATE TABLE user (uid int(11) unique NOT NULL AUTO_INCREMENT, ctime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, name varchar(50) unique NOT NULL, email varchar(50) NOT NULL, password varchar(25) NOT NULL, salt varchar(25) NOT NULL, PRIMARY KEY (uid));
+CREATE TABLE user (uid varchar(30) unique NOT NULL, ctime datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, name varchar(50) unique NOT NULL, email varchar(50) unique NOT NULL, password varchar(25) NOT NULL, salt varchar(25) NOT NULL, PRIMARY KEY (uid));
 
-create table friend_request(uid int(11), user_from varchar(30), ctime DATETIME not null DEFAULT now());
+create table friend_request(uid varchar(30), user_from varchar(30), ctime DATETIME not null DEFAULT CURRENT_TIMESTAMP); 
 
-create table friend_list(uid int(11), friend varchar(30), ctime DATETIME not null DEFAULT now(), foreign key (uid) references user(uid));
+create table friend_list(uid varchar(30), friend varchar(30), ctime DATETIME not null DEFAULT CURRENT_TIMESTAMP, foreign key (uid) references user(uid), foreign key (friend) references user(uid));
 
-create table message(uid int(11), frm varchar(30), ctime DATETIME not null DEFAULT now(), msg text, foreign key (uid) references user(uid));
+create table message(uid varchar(30), frm varchar(30), ctime DATETIME not null DEFAULT CURRENT_TIMESTAMP, msg text, foreign key (uid) references user(uid), foreign key(frm) references user(uid));
 
 insert into user set name = "admin",email = "admin@tulipsecure.com",password = "tulipPa55",salt = "verysalty";
 
