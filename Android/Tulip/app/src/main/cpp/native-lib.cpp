e2a7091a86dcad53f5c58430e1a617d56b992350
Irfan Gulamali <irfan.412@yahoo.com>
2017-11-09 23:13:10 -0500

    calling get friend list on mobile

--- Android/Tulip/app/src/main/cpp/native-lib.cpp
+++ Android/Tulip/app/src/main/cpp/native-lib.cpp
@@ -57,3 +57,41 @@ Java_org_tulip_project_tulip_Login_stringFromJNI(
     return env->NewStringUTF(hello.c_str());
 }
 
+extern "C"
+JNIEXPORT jobjectArray JNICALL
+Java_org_tulip_project_tulip_MainActivity_GetList(JNIEnv *env, jobject instance, jstring user_,
+                                                  jstring pass_) {
+    const char *user = env->GetStringUTFChars(user_, 0);
+    const char *pass = env->GetStringUTFChars(pass_, 0);
+
+    jobjectArray ret;
+    unsigned count = 0;
+    unsigned long long offset = 0;
+
+    char *list = NULL;
+    char t_str[30] = {0};
+    unsigned list_sz = 0;
+    ret= (jobjectArray)env->NewObjectArray(500,
+                                           env->FindClass("java/lang/String"),env->NewStringUTF(""));
+    client_get_friendlist((char*)user, (char*)pass, conn, &list, &list_sz, 0);
+
+    while(list_sz > 0 && count < 500)
+    {
+        count += list_sz;
+        memcpy(&offset, list, 8);
+        for(int i = 0; i < 30*list_sz; i+=30)
+        {
+            if(count +((i+30)/30) < 500)
+            {
+                memcpy(t_str, &list[8+i], 30);
+                env->SetObjectArrayElement(ret,(count+(i+30)/30),env->NewStringUTF(t_str));
+            }
+        }
+        client_get_friendlist((char*)user, (char*)pass, conn, &list, &list_sz, offset);
+    }
+
+    env->ReleaseStringUTFChars(user_, user);
+    env->ReleaseStringUTFChars(pass_, pass);
+
+    return ret;
+}
