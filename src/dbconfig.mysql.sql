5464db4073c3bda0589ea87752fccc05b3e80a72
Irfan Gulamali <irfan.412@yahoo.com>
2017-11-16 16:33:56 -0500

    refactored message table

--- src/dbconfig.mysql.sql
+++ src/dbconfig.mysql.sql
@@ -6,7 +6,7 @@ create table friend_request(rowid bigint not null auto_increment, uname varchar(
 
 create table friend_list(rowid bigint not null auto_increment, uname varchar(30), friend varchar(30), ctime DATETIME not null DEFAULT CURRENT_TIMESTAMP, foreign key (uname) references user(uname), foreign key (friend) references user(uname), PRIMARY KEY(rowid));
 
-create table message(rowid bigint not null auto_increment, uname varchar(30), frm varchar(30), ctime DATETIME not null DEFAULT CURRENT_TIMESTAMP, msg text, foreign key (uname) references user(uname), foreign key(frm) references user(uname), primary key(rowid));
+create table message(rowid bigint not null auto_increment, uname varchar(30), frm varchar(30), ctime DATETIME not null DEFAULT CURRENT_TIMESTAMP, msg text, new BOOL NOT NULL default TRUE, foreign key (uname) references user(uname), foreign key(frm) references user(uname), primary key(rowid));
 
 insert into user set name = "admin",email = "admin@tulipsecure.com",password = "tulipPa55",salt = "verysalty";
 
